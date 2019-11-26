ab949ce5e8e2726c5c1f8ffb6fb56357c933c461
COSSU_A <amandamcossu@gmail.com>
2019-11-26 11:14:20 +0100

    ride

--- sound.js
+++ sound.js
@@ -1,23 +1,10 @@
-function playSound (e){
-    const AUDIO = document.querySelector(`AUDIO[data-key="${e.keyCode}"]`);
-    const KEY = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-    if (!AUDIO) return; // stop the function from running all together
+function playSound() {
+  const KEYCODE = this.getAttribute('data-key');
+  const AUDIO = document.querySelector(`audio[data-key="${KEYCODE}"]`);
+  AUDIO.currentTime = 0; 
+  AUDIO.play();
+}
 
-    AUDIO.currentTime = 0; // rewind to the start
-    AUDIO.play();
-
-    KEY.classList.add('playing');
-  }
-  
-
-  function removeTransition (e){
-    if (e.propertyName !== 'transform') return; // skip if it's not a transform
-    this.classList.remove("playing");
-
-  }
-
-  const KEYS = document.querySelectorAll(".key");
-  KEYS.forEach(KEY => KEY.addEventListener('transitionend', removeTransition));
-
-  window.addEventListener('keydown', playSound);
+const KEYS = document.querySelectorAll(".key");
+KEYS.forEach(KEY => KEY.addEventListener('click', playSound));
